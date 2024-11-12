8d7b866620b7a86795925b2b01f99c499b30efda
oveiss <oveiss.ershadian@gmail.com>
2024-11-12 15:51:49 +0330

    .

--- src/@core/components/StatsHorizontal/Table.js
+++ src/@core/components/StatsHorizontal/Table.js
@@ -4,10 +4,12 @@ import { Fragment, useState, useEffect } from 'react'
 // ** Invoice List Sidebar
 // import Sidebar from './Sidebar'
 
+import Avatar from '@components/avatar'
 
+import pic from '../../../assets/images/avatars/1.png'
 
 // ** Table Columns
-import { columns } from './columns'
+// import { columns } from './columns'
 
 // ** Store & Actions
 // import { getAllData, getData } from '../store'
@@ -348,7 +350,91 @@ const UsersList = () => {
     {name:'asasa', family:'aawwewr3', email:'hsregesdfwe', teacher:'uuunngg' },
     {name:'asasa', family:'aawwewr3', email:'hsregesdfwe', teacher:'uuunngg' },
   ]
-
+  const columns =[
+    {
+    name: 'User',
+    sortable: true,
+    minWidth: '300px',
+    sortField: 'fullName',
+    selector: row => row.name,
+    cell: row => (
+      <div className='d-flex justify-content-left align-items-center'>
+        {/* {renderClient(row)} */}
+        <Avatar img={pic}/>
+        <div className='d-flex flex-column'>
+          {/* <Link
+            to={`/apps/user/view/${row.id}`}
+            className='user_name text-truncate text-body'
+            onClick={() => store.dispatch(getUser(row.id))}
+          > */}
+            <span className='fw-bolder'>{row.family}</span>
+          {/* </Link> */}
+          <small className='text-truncate text-muted mb-0'>{row.email}</small>
+        </div>
+      </div>
+    )
+  },
+  {
+    name: 'Role',
+    sortable: true,
+    minWidth: '172px',
+    sortField: 'role',
+    selector: row => row.teacher,
+    // cell: row => renderRole(row)
+  },
+  {
+    name: 'استاد',
+    minWidth: '138px',
+    sortable: true,
+    sortField: 'currentPlan',
+    selector: row => row.teacher,
+    // cell: row => <span className='text-capitalize'>{row.teacher}</span>
+  },
+  {
+    name: 'Actions',
+    minWidth: '100px',
+    cell: row => (
+      <div className='column-action'>
+        <UncontrolledDropdown>
+          <DropdownToggle tag='div' className='btn btn-sm'>
+            <MoreVertical size={14} className='cursor-pointer' />
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem
+              // tag={Link}
+              className='w-100'
+              // to={`/apps/user/view/${row.id}`}
+              // onClick={() => store.dispatch(getUser(row.id))}
+            >
+              <FileText size={14} className='me-50' />
+              <span className='align-middle'>Details</span>
+            </DropdownItem>
+            <DropdownItem
+              // tag='a' 
+              href='/' 
+              // className='w-100' onClick={e => e.preventDefault()}
+             >
+              <Archive size={14} className='me-50' />
+              <span className='align-middle'>Edit</span>
+            </DropdownItem>
+            {/* <DropdownItem
+              tag='a'
+              href='/'
+              className='w-100'
+              onClick={e => {
+                e.preventDefault()
+                store.dispatch(deleteUser(row.id))
+              }}
+            >
+              <Trash2 size={14} className='me-50' />
+              <span className='align-middle'>Delete</span>
+            </DropdownItem> */}
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </div>
+    )
+  }
+]
 
   return (
     <Fragment>
